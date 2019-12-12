3a612d0e411a0bc224f0264139aa7fca6ef82596
Joris Langlois <joris.langlois@knplabs.com>
2019-12-12 17:10:32 +0100

    Filter venues by category

--- src/Symfony/Filter/Venue.php
+++ src/Symfony/Filter/Venue.php
@@ -7,11 +7,19 @@ use App\Foursquare\FoursquareFilter;
 
 class Venue implements FoursquareFilter
 {
+    // @see https://developer.foursquare.com/docs/resources/categories
+    const SUPPORTED_CATEGORIES = [
+        '4d4b7105d754a06374d81259' // Nourriture
+    ];
+
     /** @var double */
     public $latitude;
 
     /** @var double */
     public $longitude;
+
+    /** @var array an array of category ids */
+    public $categories;
 
     public function __construct(Request $request) {
         $this->latitude = $request->query->get('latitude') !== null
@@ -29,10 +37,24 @@ class Venue implements FoursquareFilter
         ) {
             throw new \Exception('Latitude and longitude are mandatory parameters.');
         }
+
+        $this->categories = [];
+
+        foreach (\explode(',', $request->query->get('categories')) as $category) {
+            if (\in_array($category, self::SUPPORTED_CATEGORIES)) {
+                $this->categories[] = $category;
+            }
+        }
     }
 
     public function toQueryParameters(): string
     {
-        return \sprintf('&ll=%s,%s', $this->latitude, $this->longitude);
+        $q = \sprintf('&ll=%s,%s', $this->latitude, $this->longitude);
+
+        if (\count($this->categories) > 0) {
+            $q .= \sprintf('&categoryId=%s', \implode(',', $this->categories));
+        }
+
+        return $q;
     }
 }
